ec7b6a2f6869d2dd9d41c308eeae07aad5212962
Jan Skowronski <crianonim@gmail.com>
2019-07-23 15:10:02 +0100

    v1

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log(process.env.PORT);
+console.log(process.env);
 console.log(process.env.API_KEY);
+
+require('http').createServer((req,res)=>{
+    res.end(JSON.stringify(process.env));
+}).listen(process.env.PORT||3000)
